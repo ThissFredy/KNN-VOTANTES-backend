88182b786c7f1fee2343979cc652fd2fe85c81fb
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2025-11-10 22:41:44 -0500

    🥅 CORS updated

--- app/main.py
+++ app/main.py
@@ -41,7 +41,9 @@ app = FastAPI(
 allowed_origins = [
     "http://localhost",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000"
 ]
 
 app.add_middleware(
